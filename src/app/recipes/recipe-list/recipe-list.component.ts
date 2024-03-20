3d56978eb251010a5027297467cbb3e73387b71e
Pieter Van Overbeke <pieter.vanoverbeke@ordina.be>
2024-03-20 22:47:08 +0100

    basics commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'this is a test',
+      'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg'
+    )
+  ]
+
+}
